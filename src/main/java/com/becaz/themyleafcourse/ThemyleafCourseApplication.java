0621521d804d3017ce8d6c6e4c8e64b456b52130
babanazar <g.babanazar@gmail.com>
2020-03-25 18:43:27 +0300

    Initial commit

---
+++ src/main/java/com/becaz/themyleafcourse/ThemyleafCourseApplication.java
@@ -0,0 +1,13 @@
+package com.becaz.themyleafcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemyleafCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThemyleafCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/becaz/themyleafcourse/ThemyleafCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.becaz.themyleafcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemyleafCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
